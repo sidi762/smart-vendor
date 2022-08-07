3a49bcdda526c8dea506866bd8f37cf9f4573f68
Sidi Liang <1467329765@qq.com>
2022-08-07 23:10:02 +0800

    UI: added sound effect

--- miniprogram_ui/pages/pay/pay.js
+++ miniprogram_ui/pages/pay/pay.js
@@ -2,6 +2,7 @@
 Page({
   // 监听页面显示
   onShow: function () {
+    this.audioPlay()
     // 自动跳转到login
     setTimeout(function () {
     // 页面跳转
@@ -9,6 +10,19 @@ Page({
         delta: 2
       })
     }, 2000)
-  }
+  },
+  audioPlay(){
+    const successAudio = wx.createInnerAudioContext()
+    successAudio.src = 'https://cdn.fgprc.org.cn/vendor/payment_success_edited.mp3'
+    successAudio.obeyMuteSwitch = false
+    successAudio.play()
+    successAudio.onPlay(() => {
+        console.log('开始播放')
+      })
+    successAudio.onError((res) => {
+        console.log(res.errMsg)
+        console.log(res.errCode)
+      })
+  },
 
 })
